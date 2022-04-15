4d49ec7a88e2d04280e1ffec3e8e90300153afef
kevinNguyen101 <nguyenkevin1202@gmail.com>
2022-04-14 18:57:21 -0700

    Added opponent's UI

--- app/src/main/java/edu/up/cs301/texasHoldem/THHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/texasHoldem/THHumanPlayer.java
@@ -8,8 +8,18 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 import edu.up.cs301.game.GameFramework.GameMainActivity;
 import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
@@ -40,18 +50,27 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
     private Button bet;
     private Button fold;
     private SeekBar valueSB;
+
     private TextView valueTV;
     private TextView balanceTV;
     private TextView potTV;
     private TextView usernameTV;
     private AnimationSurface dealerAS;
     private AnimationSurface handAS;
+    private LinearLayout[] oppProfiles = new LinearLayout[3];
+    private LinearLayout yourProfile;
+
+    private ArrayList<Player> playerList = new ArrayList<>();
+    private ArrayList<CardAnimator> hands = new ArrayList<>();
+
     private int valueInt;
     private Player me; //this will make things much easier
     private CardAnimator handAnimator;
     private CardAnimator dealerAnimator;
 
+
     private THState gameState;
+    ProfileManager manager;
 
     private int backgroundColor = 0xFF35654D;
 
@@ -76,12 +95,15 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
             return;
         }
         gameState = (THState) info;
+        playerList = gameState.getPlayers();
 
         //this actually needs to get updated every time we get new info
         me = gameState.getPlayers().get(playerNum);
 
+
         if (handAnimator == null || dealerAnimator == null) {
             handAnimator = new CardAnimator(me.getHand(), "player", 0xFFFFFFFF, handAS);
+            hands.add(playerNum,handAnimator);
             dealerAnimator = new CardAnimator(gameState.getDealerHandAsArray(), "dealer",
                     backgroundColor, dealerAS);
             handAS.setAnimator(handAnimator);
@@ -89,6 +111,78 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
         }
         handAnimator.setCards(me.getHand()); //make sure we're rendering the current game state
         dealerAnimator.setCards(gameState.getDealerHandAsArray());
+
+
+
+        //Sets up the other players Information
+        int idx = 0;
+        for(int i = 0; i < playerList.size(); i++){
+            Player player = gameState.getPlayers().get(i);
+            LinearLayout layout = null;
+            //To separate which one is the player and which one is the opponent
+            if(i == playerNum){
+                //skip the player bc they'll be updated in UpdateUI
+                continue;
+            }
+            else{
+                layout = oppProfiles[idx];
+                idx++;
+            }
+
+            if(player != null) { //Makes sure that the player exist (should not trigger)
+                /**
+                 * External citation - Feb 13, 2022
+                 * problem: getting the children of the liner layout
+                 * link: https://stackoverflow.com/questions/6615723/getting-child-elements-from-linearlayout
+                 * solution: do a for loop to access all the views inside the layout
+                 */
+                int count = layout.getChildCount();
+                for (int j = 0; j < count; j++) {
+                    View id = layout.getChildAt(j);
+
+                    if(id instanceof ImageView){
+                        ((ImageView) id).setImageResource(R.drawable.cheems_avatar);
+                    }
+                    else if(id instanceof LinearLayout){
+                        //Since one of the child is also a linear layout, I have to do the same damned for loop
+                        //But this time with tag! (yay....)
+                        LinearLayout childLayout = ((LinearLayout) id);
+                        int countId = childLayout.getChildCount();
+                        for(int c = 0; c < countId; c++){
+                            View childView = childLayout.getChildAt(c);
+                            if(childView instanceof TextView) {
+                                String tag = (String) childView.getTag();
+                                if (tag.equals("name")) {
+                                    ((TextView)childView).setText(player.getName());
+                                } else if (tag.equals("action")) {
+                                    //TODO: Get the action from each player from somewhere
+                                    ((TextView)childView).setText(player.getAction());
+                                } else if (tag.equals("money")) {
+                                    ((TextView)childView).setText("$ " + player.getBalance());
+                                }
+                            }
+                            else if(childView instanceof AnimationSurface){
+                                //creates the hand if it hasn't before using playerlist as size reference
+                                //TODO: Find a way to hide the opponent's cards until the game completes
+                                if(hands.size() < playerList.size()){
+                                    hands.add(i , new CardAnimator(player.getHand(),"player",0xFFFFFF,(AnimationSurface) childView));
+                                    ((AnimationSurface) childView).setAnimator(hands.get(i));
+                                }
+                                ((AnimationSurface) childView).setAnimator(hands.get(i));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+/*
+        manager = new ProfileManager(gameState.getProfiles(),R.id.profileList);
+        for(int  i = 0; i < gameState.getPlayers().size(); i++){
+            if(playerNum != i) {
+                manager.addProfiles(i);
+            }
+        }
+*/
 
         updateUI();
     }
@@ -119,6 +213,16 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
         this.handAS = activity.findViewById(R.id.playerHandAS);
         this.usernameTV = activity.findViewById(R.id.userID);
 
+        this.yourProfile = activity.findViewById(R.id.playerProfile);
+        this.oppProfiles[0] = activity.findViewById(R.id.oppProfile1);
+        this.oppProfiles[1] = activity.findViewById(R.id.oppProfile2);
+        this.oppProfiles[2] = activity.findViewById(R.id.oppProfile3);
+
+        //Add the views of the opponent's profiles (Temporary until alternatives work)
+
+
+
+
         //Listen for button presses
         bet.setOnClickListener(this);
         fold.setOnClickListener(this);
@@ -144,6 +248,9 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
         String value = ""+(int) ((gameState.getCurrentBet()-me.getBet())
                 +(me.getBalance()*((float) valueSB.getProgress()/valueSB.getMax())));
         valueTV.setText(value);
+
+        //Updates names and actions of the opponents
+
 
         //TODO: animationSurfaces
     }
