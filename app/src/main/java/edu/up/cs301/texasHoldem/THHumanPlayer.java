5208ccbb8ffd082bd1254715cb76b49c412b7846
kevinNguyen101 <nguyenkevin1202@gmail.com>
2022-04-20 17:16:18 -0700

    Design update

--- app/src/main/java/edu/up/cs301/texasHoldem/THHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/texasHoldem/THHumanPlayer.java
@@ -113,7 +113,7 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
         me = gameState.getPlayers().get(playerNum);
 
         if (handAnimator == null || dealerAnimator == null) {
-            handAnimator = new CardAnimator(me.getHand(), 0xFFFFFFFF, handAS, myActivity);
+            handAnimator = new CardAnimator(me.getHand(), backgroundColor, handAS, myActivity);
             hands.add(playerNum,handAnimator);
             dealerAnimator = new CardAnimator(gameState.getDealerHandAsArray(),
                     backgroundColor, dealerAS, myActivity);
@@ -175,7 +175,7 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
                                 //creates the hand if it hasn't before using playerlist as size reference
                                 //TODO: Find a way to hide the opponent's cards until the game completes
                                 if(hands.size() < playerList.size()){
-                                    hands.add(i , new CardAnimator(player.getHand(),0xFFFFFF,
+                                    hands.add(i , new CardAnimator(player.getHand(),backgroundColor,
                                             (AnimationSurface) childView, myActivity));
                                     ((AnimationSurface) childView).setAnimator(hands.get(i));
                                 }
@@ -263,7 +263,7 @@ public class THHumanPlayer extends GameHumanPlayer implements View.OnClickListen
         usernameTV.setText(me.getName());
 
         //(current min bet - our current bet) + ( our balance * ( seekbar value / seekbar max ) )
-        valueTV.setText(""+getSliderBet());
+        valueTV.setText("$"+getSliderBet());
 
 
 
